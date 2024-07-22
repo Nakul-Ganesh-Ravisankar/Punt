302b997b856ce6a884123bae23b65209397bcaed
Nakul-Ganesh-Ravisankar <125003454@sastra.ac.in>
2024-07-22 16:21:17 +0530

    Update Translation1.java

--- Translation1.java
+++ Translation1.java
@@ -4,37 +4,37 @@ import com.google.cloud.speech.v1.*;
 import com.google.cloud.texttospeech.v1.*;
 import com.google.protobuf.ByteString;
 
-public class TranslationService {
-    private static Translate translate;
-    private static SpeechClient speechClient;
-    private static TextToSpeechClient textToSpeechClient;
+public class Translation1 {
+    private static Translate trans;
+    private static SpeechClient sClient;
+    private static TextToSpeechClient ttsClient;
 
-    public static void setTranslate(Translate translate) {
-        TranslationService.translate = translate;
+    public static void setTranslate(Translate trans) {
+        Translation1.trans = trans;
     }
 
-    public static void setSpeechClient(SpeechClient speechClient) {
-        TranslationService.speechClient = speechClient;
+    public static void setSpeechClient(SpeechClient sClient) {
+        Translation1.sClient = sClient;
     }
 
-    public static void setTextToSpeechClient(TextToSpeechClient textToSpeechClient) {
-        TranslationService.textToSpeechClient = textToSpeechClient;
+    public static void setTextToSpeechClient(TextToSpeechClient ttsClient) {
+        Translation1.ttsClient = ttsClient;
     }
 
     public static String detectLanguage(String input) {
-        Detection detection = translate.detect(input);
-        return detection.getLanguage();
+        Detection detect1 = translate.detect(input);
+        return detect1.getLanguage();
     }
 
     public static String translateText(String input, String targetLanguage) {
-        Translation translation = translate.translate(input, Translate.TranslateOption.targetLanguage(targetLanguage));
-        return translation.getTranslatedText();
+        Translation trans1 = translate.translate(input, Translate.TranslateOption.targetLanguage(targetLanguage));
+        return trans1.getTranslatedText();
     }
 
     public static String speechToText(ByteString audioBytes) throws Exception {
         RecognitionAudio audio = RecognitionAudio.newBuilder().setContent(audioBytes).build();
         RecognitionConfig config = RecognitionConfig.newBuilder().setEncoding(RecognitionConfig.AudioEncoding.LINEAR16).setSampleRateHertz(16000).setLanguageCode("en-US").build();
-        RecognizeResponse response = speechClient.recognize(config, audio);
+        RecognizeResponse response = sClient.recognize(config, audio);
         return response.getResultsList().get(0).getAlternativesList().get(0).getTranscript();
     }
 
@@ -42,7 +42,7 @@ public class TranslationService {
         SynthesisInput inputText = SynthesisInput.newBuilder().setText(input).build();
         VoiceSelectionParams voice = VoiceSelectionParams.newBuilder().setLanguageCode(languageCode).setSsmlGender(SsmlVoiceGender.NEUTRAL).build();
         AudioConfig audioConfig = AudioConfig.newBuilder().setAudioEncoding(AudioEncoding.LINEAR16).build();
-        SynthesizeSpeechResponse response = textToSpeechClient.synthesizeSpeech(inputText, voice, audioConfig);
+        SynthesizeSpeechResponse response = ttsClient.synthesizeSpeech(inputText, voice, audioConfig);
         return response.getAudioContent();
     }
 }
